2183f73a36f4aa9249f212e241cc278079b6f5ab
Aristote <jme@acklabs.net>
2013-12-10 13:10:34 +0100

    Initial Commit

---
+++ afibosx/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  afibosx
+//
+//  Created by jme on 10/12/2013.
+//  Copyright (c) 2013 jme. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
